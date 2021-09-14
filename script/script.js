0ef35c82e462307f395e9c4c7d3175cc1611dd66
Yanina Bucca <Yanina@MacBook-Air-de-mac.local>
2021-09-13 21:00:01 -0300

    add button page

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ const marvel = {
 // 18317076127f02840e2a8ace0207c5d934fe6632e9ca1976d23ace42021fea1ba2225b7bd
 
   render:() => {
-    const urlAPI ="https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9ca1976d23ace42021fea1ba2225b7bd&hash=3722a07e43de87305375496f55f99f7a";
+    const urlAPI ="https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=9ca1976d23ace42021fea1ba2225b7bd&hash=3722a07e43de87305375496f55f99f7a&offset=20&limit=20";
     const container = document.querySelector(`#marvel-row`);
     let contentHTML="";
 
@@ -36,3 +36,4 @@ const marvel = {
    }
 };
 marvel.render();
+
